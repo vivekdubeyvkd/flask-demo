5c77e8aa1f4f631b04c1c3827a414309951e33e1
vivekdubeyvkd <vivekdubeyvkd@gmail.com>
2019-03-29 12:47:04 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+from lib.lib import returnInfo
+
+app = Flask(__name__)
+  
+@app.route("/")
+def main():
+    #return "Welcome!"
+	return render_template('index.html')
+	
+@app.route('/queryInfo')
+def queryInfo():
+    return render_template('queryInfo.html')
+	
+@app.route('/getInfo',methods=['POST' , 'GET'])
+def getinfo():
+    _key = request.form['repo']
+    return returnInfo(_key)
+	
+if __name__ == "__main__":
+    app.run()
